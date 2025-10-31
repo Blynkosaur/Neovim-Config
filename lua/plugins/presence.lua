bb2d2a64cf7b47b821fe03746d80eb12c800c5db
mini-bryanlin <bryanlin316@gmail.com>
2025-10-31 00:10:12 -0400

    discord

---
+++ lua/plugins/presence.lua
@@ -0,0 +1,19 @@
+return {
+  -- Other plugins here
+
+  {
+    "andweeb/presence.nvim",
+    config = function()
+      require("presence").setup({
+        -- Your configuration here, for example:
+        auto_update = true,
+        neovim_image_text = "The One True Text Editor",
+        main_image = "neovim",
+        client_id = "793271441293967371",
+        debounce_timeout = 10,
+        buttons = true,
+        show_time = true,
+      })
+    end,
+  },
+}
