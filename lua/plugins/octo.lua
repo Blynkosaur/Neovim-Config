65b7dc56246b1ddd35c3d65fc7d632fe927a0c46
Bryan Lin <bryanlin316@gmail.com>
2026-06-25 12:49:51 -0400

    feat: added octo.lua

---
+++ lua/plugins/octo.lua
@@ -0,0 +1,12 @@
+return {
+  "pwntester/octo.nvim",
+  opts = {
+    default_to_projects_v2 = true,
+    default_merge_method = "squash",
+    enable_builtin = true,
+  },
+  keys = {
+    -- add or remap commands here, e.g.
+    { "<leader>go", "<cmd>Octo<cr>", desc = "Octo menu" },
+  },
+}
