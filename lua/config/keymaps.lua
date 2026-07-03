da070766cc5c9be46b2991664afb3b21d9111ca0
mini-bryanlin <bryanlin316@gmail.com>
2026-07-03 13:16:00 -0400

    feat: added c-f L and H to resize neovim panes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 -- jk to escape in insert mode
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- <C-f>l / <C-f>h — push the vertical split border right/left by 5 columns,
+-- mirroring tmux prefix + l/h (resize-pane -R/-L 5)
+local function resize_border(dir)
+  local has_window_to_right = vim.fn.winnr("l") ~= vim.fn.winnr()
+  local grow = (dir == "right") == has_window_to_right
+  vim.cmd("vertical resize " .. (grow and "+5" or "-5"))
+end
+
+vim.keymap.set("n", "<C-f>l", function() resize_border("right") end, { desc = "Expand window right" })
+vim.keymap.set("n", "<C-f>h", function() resize_border("left") end, { desc = "Expand window left" })
